b87d29ffe6aebf4f056fcce3793dfbb47d5f6769
Tigran <1993tiko@mail.ru>
2019-01-08 17:42:56 +0400

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,17 +1,12 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=eworks_aplusbe',
-    'username' => 'eworks_aplusbe',
-    'password' => 'x?mDQMWIT1]L',
-    'charset' => 'utf8',
 
-//    'class' => 'yii\db\Connection',
-//    'dsn' => 'mysql:host=localhost;dbname=apb',
-//    'username' => 'root',
-//    'password' => '',
-//    'charset' => 'utf8',
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=apb',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
